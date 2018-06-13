4470213afa877e3e228e2b1979ff7027a53f581e
S.O <shakirat.odunsi@gmail.com>
2018-06-13 16:30:08 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,11 +1,11 @@
-def display_rainbow (test)
-puts "R: #{test[0]}"
-puts "O #{test[1]}"
-puts "Y: #{test[2]}"
-puts "G: #{test[3]}"
-puts "B: #{test[4]}"
-puts "I: #{test[5]}"
-puts "V: #{test[6]}"
+def display_rainbow (colors)
+puts "R: #{colors[0]}"
+puts "O #{colors[1]}"
+puts "Y: #{colors[2]}"
+puts "G: #{colors[3]}"
+puts "B: #{colors[4]}"
+puts "I: #{colors[5]}"
+puts "V: #{colors[6]}"
 end
 color=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 display_rainbow(color)
