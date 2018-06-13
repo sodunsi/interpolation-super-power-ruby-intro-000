978007fe24205816bc06bcd664aaddb5d3c351bb
S.O <shakirat.odunsi@gmail.com>
2018-06-13 16:01:13 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,10 +1,10 @@
-def display_rainbow (R:,O:,Y:,G:,B:,I:,V:)
-  puts red
-  puts orange
-  puts yellow
-  puts green
-  puts blue
-  puts indigo
-  puts violet
+def display_rainbow ("r","o","y","g","b","i","v")
+puts "The first color is: #{display_rainbow[0]}"
+puts "The second color: #{display_rainbow[1]}"
+puts "The third color is: #{display_rainbow[2]}"
+puts "The forth color is: #{display_rainbow[3]}"
+puts "The fifth color is: #{display_rainbow[4]}"
+puts "The sixth color is: #{display_rainbow[5]}"
+puts "The seventh color is: #{display_rainbow[6]}"
 end
-display_rainbow R:"red", O:"orange", Y:"yellow", G:"green", B:"blue", I:"indigo", V:"violet"
+display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
