06ee808967c94a31ba3d219875e433383c4c6f0f
S.O <shakirat.odunsi@gmail.com>
2018-06-13 16:24:17 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,7 +3,7 @@ puts "The first color is: #{test[0]}"
 puts "The second color: #{test[1]}"
 puts "The third color is: #{test[2]}"
 puts "The forth color is: #{test[3]}"
-puts "The fifth color is: #{test]}"
+puts "The fifth color is: #{test[4]}"
 puts "The sixth color is: #{test[5]}"
 puts "The seventh color is: #{test[6]}"
 end
